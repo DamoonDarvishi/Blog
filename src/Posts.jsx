6ebc3c3c8064c18b804c0039ed15a1472b48312d
damoondarvishi <damoon.darvishi@gmail.com>
2021-09-03 21:23:27 +0430

    useQuery, useMutation and prefetching

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,21 +1,33 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { useQuery, useQueryClient } from "react-query";
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(pageNum) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+  console.log(queryClient);
+  useEffect(() => {
+    const nextPage = currentPage + 1
+    queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+  }, [currentPage, queryClient]);
+
   // replace with useQuery
-  const data = [];
+  const { data, isLoading, isError, error } = useQuery(["posts", currentPage], () => fetchPosts(currentPage), {
+    staleTime: 2000
+  });
+  // if(!data) return <div />
+  if (isLoading) return <h3>Loading ...</h3>
+  if (isError) return <><h3>Oops, something went wrong</h3><h2>{error.toString()}</h2></>
 
   return (
     <>
@@ -31,11 +43,11 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button disabled={ currentPage <=1 } onClick={() => { setCurrentPage((previousValue) => previousValue - 1)}}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage }</span>
+        <button disabled={ currentPage >= maxPostPage } onClick={() => { setCurrentPage((previousValue) => previousValue + 1)}}>
           Next page
         </button>
       </div>
